414cf6fd08daf822f71e1e660e645361eb5945e4
tanphandev <phantanbn31@gmail.com>
2024-05-16 17:29:39 +0700

    fix: imporve ui

--- src/components/sidebar/channel-item.tsx
+++ src/components/sidebar/channel-item.tsx
@@ -17,7 +17,7 @@ function ChannelItem({ room }: Props) {
       className="transition-all py-4 px-2 flex items-center gap-2 hover:bg-[#eaf3ff] rounded-lg cursor-pointer"
     >
       <Image src={avatar} alt="avatar" width={0} height={0} className="rounded-full w-14 h-14" />
-      <div className="flex flex-col">
+      <div className="sm:flex flex-col hidden">
         <h3 className="text-lg font-semibold">{room?.name}</h3>
         <div className="opacity-50 line-clamp-1">{room?.messages?.[room?.messages?.length - 1]?.body ?? ""}</div>
       </div>
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -8,10 +8,10 @@ function Sidebar() {
   const { rooms } = useChatContext();
   console.log("rooms", rooms);
   return (
-    <div className="pt-5 px-4 w-[360px] h-[calc(100vh-64px)] flex flex-col">
+    <div className="pt-5 px-4 w-auto sm:w-[360px] h-[calc(100vh-64px)] flex flex-col">
       <div>
         <div className="text-2xl font-bold">Chats</div>
-        <div className="flex text-base p-[10px] rounded-full bg-[#f0f2f5] mb-2">
+        <div className="sm:flex text-base p-[10px] rounded-full bg-[#f0f2f5] mb-2 hidden">
           <Image src={searchIcon} alt="search-icon" width={16} height={16} className="mr-2" />
           <input className="font-semibold bg-transparent outline-none" type="text" placeholder="Search Messenger" />
         </div>
